a48f1efff9aba05d7d668085de539da1e4915f66
Lemmo Lavonen <lemmo.lavonen@gmail.com>
2021-10-19 00:33:32 +0300

    Add a method for signing the auth token hash.

--- app/src/main/java/ee/ut/math/lemmo/readnfctag/MainActivity.java
+++ app/src/main/java/ee/ut/math/lemmo/readnfctag/MainActivity.java
@@ -6,14 +6,8 @@ import android.nfc.NfcAdapter;
 import android.nfc.NfcManager;
 import android.nfc.tech.IsoDep;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import java.io.ByteArrayInputStream;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
 
 public class MainActivity extends Activity {
 
@@ -24,43 +18,40 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.response);
-        EditText editText = findViewById(R.id.CAN);
-        Button button = findViewById(R.id.connect);
-
-        button.setOnClickListener(view -> textView.setText("Card not found."));
+        EditText CAN = findViewById(R.id.CAN);
+        EditText PIN = findViewById(R.id.PIN);
 
         NfcManager manager = (NfcManager) getSystemService(Context.NFC_SERVICE);
         NfcAdapter nfcAdapter = manager.getDefaultAdapter();
         if (nfcAdapter != null) {
             nfcAdapter.enableReaderMode(this, discoveredTag -> {
 
-                runOnUiThread(() -> textView.setText("Card found."));
-                button.setOnClickListener(view -> new Thread(() -> {
+                runOnUiThread(() -> textView.setText(R.string.card_found));
+                new Thread(() -> {
 
                     try (IsoDep idCard = IsoDep.get(discoveredTag)) {
 
                         idCard.setTimeout(Short.MAX_VALUE);
 
-                        Comms comms = new Comms(idCard, editText.getText().toString());
+                        Comms comms = new Comms(idCard, CAN.getText().toString());
 
-//                        String[] response = comms.readPersonalData(new byte[]{2});
-//                        String welcome = String.format("Hello, %s.", response[0].charAt(0) + response[0].substring(1).toLowerCase());
-//                        runOnUiThread(() -> textView.setText(welcome));
+                        String[] response = comms.readPersonalData(new byte[]{2});
+                        String welcome = String.format("Hello, %s.", response[0].charAt(0) + response[0].substring(1).toLowerCase());
+                        runOnUiThread(() -> textView.setText(welcome));
 
-//                        long start = System.currentTimeMillis();
-                        byte[] response = comms.getAuthenticationCertificate();
-//                        Log.i("duration", String.valueOf(System.currentTimeMillis() - start));
-
-                        CertificateFactory certificateFactory = CertificateFactory.getInstance("X509");
-                        X509Certificate x509Certificate = (X509Certificate) certificateFactory.generateCertificate(new ByteArrayInputStream(response));
-                        Log.i("Certificate subject", x509Certificate.getSubjectX500Principal().getName());
+//                        byte[] certificate = comms.getCertificate(true);
+//                        CertificateFactory certificateFactory = CertificateFactory.getInstance("X509");
+//                        X509Certificate x509Certificate = (X509Certificate) certificateFactory.generateCertificate(new ByteArrayInputStream(certificate));
+//                        Log.i("Certificate subject", x509Certificate.getSubjectX500Principal().getName());
+//
+//                        comms.authenticate(PIN.getText().toString(), new byte[16]);
 
                     } catch (Exception e) {
                         e.printStackTrace();
                         runOnUiThread(() -> textView.setText(e.getMessage()));
                     }
 
-                }).start());
+                }).start();
 
             }, NfcAdapter.FLAG_READER_NFC_A, null);
         }
